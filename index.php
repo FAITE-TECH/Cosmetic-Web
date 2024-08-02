72def1b6d72b9defedf950483aa62a4eea7a0ce0
Sanjana Nimesh <sanjana.nim2001@gmail.com>
2024-08-02 21:40:26 +0530

    verlag added

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Beauty Skin Care</title>
     <link rel="stylesheet" href="styles.css">
+    <link href="https://db.onlinewebfonts.com/c/26ffd45d7739310d9dd0f8bf7c513625?family=Verlag-Black" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
 </head>
 <body>
