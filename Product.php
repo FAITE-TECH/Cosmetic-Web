f58c96443fd04affd234f222961ed5ced2cbc7a0
Ishalini <87859854+IT20134280@users.noreply.github.com>
2024-09-22 20:55:25 +0530

    changes done

--- Product.php
+++ Product.php
@@ -58,14 +58,18 @@ $username = $isLoggedIn ? $_SESSION['Usname'] : '';
         @media (max-width: 768px) {
             .cleansers-grid {
                 grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+                padding: 0 25px;
             }
+            
         }
 
         @media (max-width: 576px) {
             .cleansers-grid {
                 grid-template-columns: 1fr;
+                padding: 0 20px;
             }
         }
+
     </style>
 </head>
 <body>
@@ -164,8 +168,8 @@ $username = $isLoggedIn ? $_SESSION['Usname'] : '';
     <a href="https://www.instagram.com/sulosowshadham?igsh=YzljYTk1ODg3Zg==" target="_blank" rel="noopener noreferrer">
         <i class="fab fa-instagram"></i>
     </a>
-    <a href="#" target="_blank" rel="noopener noreferrer">
-        <i class="fab fa-youtube"></i>
+    <a href="https://wa.me/message/WVFLY3HNQOMCB1" target="_blank" rel="noopener noreferrer">
+        <i class="fab fa-whatsapp"></i>
     </a>
 </div>
     </div>
